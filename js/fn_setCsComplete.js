55845ee0677f4a76cde6838d21deabbdda06e3ac
nezz <imtaehyun@me.com>
2015-10-02 10:52:35 +0900

    자동 댓글 기능 추가

--- js/fn_setCsComplete.js
+++ js/fn_setCsComplete.js
@@ -2,3 +2,7 @@ $("#issue_assigned_to_id").val($(".author a").attr("href").split("/")[3]);
 $("#issue_status_id").val("15");
 $('#relation_issue_to_id').val(csNo);
 $('#new-relation-form input[name=commit]').click();
+$('#update').show();
+$('#issue_notes').focus();
+$('html, body').animate({scrollTop: $('#update').offset().top}, 100);
+if ($("#issue_notes").val() == "") {$("#issue_notes").val("* 처리되었습니다.")};
